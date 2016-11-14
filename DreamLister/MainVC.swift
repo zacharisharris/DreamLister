5e9030a380f1aa1d56add6dfc017e47ba32add5c
Harris Zacharis <zacharisharris@gmail.com>
2016-11-14 16:35:53 +0200

    Created UI & data model.

--- DreamLister/MainVC.swift
+++ DreamLister/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  DreamLister
 //
 //  Created by Harris Zacharis on 14/11/16.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ DreamLister/MaterialView.swift
@@ -0,0 +1,50 @@
+//
+//  MaterialView.swift
+//  DreamLister
+//
+//  Created by Harris Zacharis on 14/11/16.
+//  Copyright © 2016 Harris Zacharis. All rights reserved.
+//
+
+import UIKit
+
+private var materialKey = false
+
+extension UIView {
+    
+    @IBInspectable var materialDesign: Bool {
+        
+        
+        get {
+            return materialKey
+        }
+        
+        set {
+            materialKey = newValue
+            
+            if materialKey {
+                
+                self.layer.masksToBounds = false
+                self.layer.cornerRadius = 3.0
+                self.layer.shadowOpacity = 0.8
+                self.layer.shadowRadius = 3.0
+                self.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+                self.layer.shadowColor = UIColor(red: 157/255, green: 157/255, blue: 157/255, alpha: 1.0).cgColor
+            } else {
+                self.layer.cornerRadius = 0
+                self.layer.shadowOpacity = 0
+                self.layer.shadowRadius = 0
+                self.layer.shadowColor = nil
+            }
+        }
+    }
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
